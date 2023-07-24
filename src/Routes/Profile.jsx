b6226eaf04e7c90997fb332d85bf19aa63f0c485
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-24 19:43:10 +0500

    Add profile page

---
+++ src/Routes/Profile.jsx
@@ -0,0 +1,13 @@
+const Profile = () => (
+  <div className="container">
+    <div className="row">
+      <div className="col-6">
+        <h3>Missions</h3>
+      </div>
+      <div className="col-6">
+        <h3>Rockets</h3>
+      </div>
+    </div>
+  </div>
+);
+export default Profile;
