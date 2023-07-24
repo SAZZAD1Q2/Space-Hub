2d12f8390e2c9ef75b2f1488731272479b1ce6ba
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-24 19:49:38 +0500

    Add styles to Profile page

--- src/Routes/Profile.jsx
+++ src/Routes/Profile.jsx
@@ -1,11 +1,19 @@
 const Profile = () => (
-  <div className="container">
-    <div className="row">
+  <div className="container py-2">
+    <div className="row py-5">
       <div className="col-6">
-        <h3>Missions</h3>
+        <h4>My Missions</h4>
+        <ul className="list-group ">
+          <li className="list-group-item">No missions</li>
+        </ul>
       </div>
       <div className="col-6">
-        <h3>Rockets</h3>
+        <h4>My Rockets</h4>
+        <ul className="list-group">
+          <li className="list-group-item">
+            No rockets
+          </li>
+        </ul>
       </div>
     </div>
   </div>
