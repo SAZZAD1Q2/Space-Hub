59501e6f6a82952e42edf01c8cf433cb94593d29
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-24 19:42:41 +0500

    Add navbar Component

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => (
+  <nav>
+    <ul>
+      <li>
+        <NavLink to="/">Rockets</NavLink>
+      </li>
+      <li>
+        <NavLink to="/missions">Missions</NavLink>
+      </li>
+      <li>
+        <NavLink to="/profile">Profile </NavLink>
+      </li>
+    </ul>
+  </nav>
+);
+export default Navbar;
