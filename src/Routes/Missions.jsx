ebb57eb3a51a332a751b81ff64bf231f5eb09091
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-24 23:23:21 +0500

    Fetch data from API

--- src/Routes/Missions.jsx
+++ src/Routes/Missions.jsx
@@ -1,2 +1,24 @@
-const Missions = () => <div>Missions</div>;
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMissions } from '../Redux/Missions/missionSlice';
+
+const Missions = () => {
+  let missionsData = useSelector((state) => state.missions.missions);
+  const localMissions = JSON.parse(localStorage.getItem('missions')) || [];
+  if (localMissions.length > 0) {
+    missionsData = localMissions;
+  }
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (missionsData.length === 0) {
+      dispatch(fetchMissions());
+    }
+  });
+  return (
+
+    <div className="container my-5">
+      <div className="table-responsive table-sm" />
+    </div>
+  );
+};
 export default Missions;
