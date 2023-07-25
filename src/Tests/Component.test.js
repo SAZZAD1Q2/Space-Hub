d27a3ac8ce4e413a0b05555279272acaa332ed8d
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-26 03:02:08 +0500

    Solve linter error

--- src/Tests/Component.test.js
+++ src/Tests/Component.test.js
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from '../Redux/store';
+import store from '../Redux/Store';
 import Missions from '../Components/Missions';
 import Rocket from '../Components/Rocket';
 import Profile from '../Components/Profile';
