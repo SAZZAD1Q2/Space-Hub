09335cdd7598ba06f33f1f5e37c06b1a4fca5643
SAZZAD1Q2 <kurulia115@gmail.com>
2023-07-25 19:26:59 +0600

    rocket data fetched

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionReducer from './Missions/missionSlice';
+import rocketReducer from './Rocket/rocketSlice';
 
 const Store = configureStore({
   reducer: {
     missions: missionReducer,
+    rockets: rocketReducer,
   },
 });
 
